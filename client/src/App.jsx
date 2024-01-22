943519d8e04d8106cb227d10552d3b064bdd7e80
Gary Smith <garysmith193358@gmail.com>
2024-01-22 16:16:56 -0500

    created front end to grab fact from backend

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,29 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import './App.css'
+import axios from "axios";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [fact, setFact] = useState("")
+
+    useEffect(() => {
+        const fetchFact = async () => {
+            try {
+                const response = await axios.get("http://localhost:8080/");
+                const data = response.data;
+                console.log(data)
+                setFact(data)
+            } catch (e) {
+                console.log(e)
+                setFact("There is an error")
+            }
+        }
+        fetchFact();
+    }, []);
 
   return (
     <>
      <h1>This is your random fact of the day:</h1>
+        <p> {fact}</p>
     </>
   )
 }
